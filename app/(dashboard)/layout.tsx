d8fa3c1cd4537595dec71e6af9a11b4e4a7a4f14
Sawat <saswatranjan0602@gmail.com>
2024-04-10 20:28:26 +0530

    made some component s

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,17 @@
+
+import Sidebar from "./_components/Sidebar";
+import React from "react";
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  console.log("object")
+
+  return (
+    <div className="h-full">
+      <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
+        <Sidebar />
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default DashboardLayout;
---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>PRRRRRRRRRRRRRRRRRRR</div>
+  )
+}
+
+export default page
