2eebdd815ffa3dbc9b7154fac2199354368f6d76
Sawat <saswatranjan0602@gmail.com>
2024-04-12 22:54:16 +0530

    dummy component

---
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -0,0 +1,10 @@
+
+
+const SearchPage =()=>{
+    return(
+        <div>
+            This is seachPage!
+        </div>
+    )
+}
+export default SearchPage
---
+++ app/(dashboard)/_components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='p-4 border-b h-full items-center bg-white shadow-sm'>
+        
+    </div>
+  )
+}
+
+export default Navbar
